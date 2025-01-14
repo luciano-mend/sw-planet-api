c220f4c9a840595533c02f5e361e043583698ef9
Luciano Mendonca <luciano.s.mend@gmail.com>
2025-01-14 19:08:49 -0300

    commit inicial

---
+++ src/main/java/br/luciano/sw_planet_api/domain/PlanetRepository.java
@@ -0,0 +1,7 @@
+package br.luciano.sw_planet_api.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PlanetRepository extends CrudRepository<Planet, Long> {
+
+}
