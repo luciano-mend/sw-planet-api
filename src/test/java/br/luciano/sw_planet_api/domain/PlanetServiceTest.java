178a8e59fdcc5f4028193b730909bd5bf6f9146a
Luciano Mendonca <luciano.s.mend@gmail.com>
2025-01-14 22:47:28 -0300

    AssertThatThrownBy - aguardando uma exception

--- src/test/java/br/luciano/sw_planet_api/domain/PlanetServiceTest.java
+++ src/test/java/br/luciano/sw_planet_api/domain/PlanetServiceTest.java
@@ -1,7 +1,9 @@
 package br.luciano.sw_planet_api.domain;
 
 import static br.luciano.sw_planet_api.common.PlanetConstants.PLANET;
+import static br.luciano.sw_planet_api.common.PlanetConstants.INVALID_PLANET;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
@@ -27,5 +29,12 @@ public class PlanetServiceTest {
         Planet sut = planetService.create(PLANET);
 
         assertThat(sut).isEqualTo(PLANET);
+    }
+
+    @Test
+    public void createPlanet_WithInvalidData_ThrowsException() {
+        when(planetRepository.save(INVALID_PLANET)).thenThrow(RuntimeException.class);
+
+        assertThatThrownBy(() -> planetService.create(INVALID_PLANET)).isInstanceOf(RuntimeException.class);
     }
 }
