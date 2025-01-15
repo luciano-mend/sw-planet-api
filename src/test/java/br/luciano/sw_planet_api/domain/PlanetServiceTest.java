83e5eaccbd65377faa6295393f48854b540a1206
Luciano Mendonca <luciano.s.mend@gmail.com>
2025-01-14 22:27:01 -0300

    SpringBootTest, MockitoBean -> Mockito

--- src/test/java/br/luciano/sw_planet_api/domain/PlanetServiceTest.java
+++ src/test/java/br/luciano/sw_planet_api/domain/PlanetServiceTest.java
@@ -3,18 +3,33 @@ package br.luciano.sw_planet_api.domain;
 import static br.luciano.sw_planet_api.common.PlanetConstants.PLANET;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-@SpringBootTest(classes = PlanetService.class)
+@ExtendWith(MockitoExtension.class)
+// @SpringBootTest(classes = PlanetService.class)
 public class PlanetServiceTest {
-    @Autowired
+    // @Autowired
+    @InjectMocks
     private PlanetService planetService;
+
+    // @MockitoBean
+    @Mock
+    private PlanetRepository planetRepository;
     
     //operacao_estado_retorno
     @Test
     public void createPlanet_WithValidData_ReturnsPlanet() {
+        when(planetRepository.save(PLANET)).thenReturn(PLANET);
+
         //SUT System Under Test
         Planet sut = planetService.create(PLANET);
 
