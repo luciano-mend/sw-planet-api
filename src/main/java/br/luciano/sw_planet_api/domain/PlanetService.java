ca722e7b35e22489af5981c8f10b3de5acfe8939
Luciano Mendonca <luciano.s.mend@gmail.com>
2025-01-18 09:18:08 -0300

    inclusão da rota get por id

--- src/main/java/br/luciano/sw_planet_api/domain/PlanetService.java
+++ src/main/java/br/luciano/sw_planet_api/domain/PlanetService.java
@@ -1,5 +1,7 @@
 package br.luciano.sw_planet_api.domain;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,5 +14,9 @@ public class PlanetService {
 
     public Planet create(Planet planet){
         return planetRepository.save(planet);
+    }
+
+    public Optional<Planet> get(Long id) {
+        return planetRepository.findById(id);
     }
 }
--- src/main/java/br/luciano/sw_planet_api/web/PlanetController.java
+++ src/main/java/br/luciano/sw_planet_api/web/PlanetController.java
@@ -1,16 +1,18 @@
 package br.luciano.sw_planet_api.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.luciano.sw_planet_api.domain.Planet;
 import br.luciano.sw_planet_api.domain.PlanetService;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
@@ -19,11 +21,16 @@ public class PlanetController {
     @Autowired
     private PlanetService planetService;
 
-    @PostMapping("path")
+    @PostMapping
     public ResponseEntity<Planet> create(@RequestBody Planet planet) {
         Planet planetCreated = planetService.create(planet);
         return ResponseEntity.status(HttpStatus.CREATED).body(planetCreated);
     }
     
+    @GetMapping("/{id}")
+    public ResponseEntity<Planet> get(@PathVariable("id") Long id) {
+        return planetService.get(id).map(planet -> ResponseEntity.ok(planet))
+            .orElseGet(() -> ResponseEntity.notFound().build());
+    }
     
 }
